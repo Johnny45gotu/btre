9d2c14420490db86cb2dbc2972fbed91eb3e4098
Jonathan <johnny45gotu@outlook.com>
2020-04-02 15:14:20 -0400

    completed app with dashboard complete

--- accounts/views.py
+++ accounts/views.py
@@ -73,6 +73,8 @@ def logout(request):
 def dashboard(request):
     user_contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
 
-    
+    context = {
+        'contacts': user_contacts
+    }
 
-    return render(request, 'accounts/dashboard.html')
+    return render(request, 'accounts/dashboard.html', context)
